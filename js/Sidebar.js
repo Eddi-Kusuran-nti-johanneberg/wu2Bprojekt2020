70c024d4d2ccb14794c3e15a41c6b8e7dbb384ee
Eddi-Kusuran-nti-johanneberg <eddi.kusuran@elev.ga.ntig>
2020-03-11 15:01:38 +0100

    Fortsättning på mobile

---
+++ js/Sidebar.js
@@ -0,0 +1,13 @@
+function showMenu(){
+    let menu = document.querySelector(".menu");
+    menu.classList.toggle("show");
+
+    let fade = document.querySelector(".fade-layer");
+    fade.classList.toggle("visible");
+}
+
+let button = document.querySelector("#Sidebar");
+let layer = document.querySelector(".fade-layer");
+
+button.addEventListener("click",showMenu);
+layer.addEventListener("click",showMenu);
